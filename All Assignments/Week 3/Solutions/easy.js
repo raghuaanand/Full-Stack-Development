9716b90762bfc17f490aae8ef8ff2a61b5617044
Raghu Anand <raghuaanand@gmail.com>
2024-01-19 14:00:18 +0530

    course selling user side added

--- All Assignments/Week 3/Solutions/easy.js
+++ All Assignments/Week 3/Solutions/easy.js
@@ -25,6 +25,22 @@ const adminAuthentication = (req, res, next) => {
     }
 }
 
+
+// MIDDLEWARE FOR USER AUTHENTICATION
+
+const userAuthentication = (req, res, next) => {
+    const {username, password} = req.headers;
+
+    const user = USERS.find(u => u.password === password && u.username === username);
+    if(user){
+        req.user = user; // adds user object to the request
+        next();
+    }else{
+        res.status(403).json({message:'User Authentication Failed'})
+    }
+}
+
+/* ---------------------------ADMIN----------------------- */
 //  SIGNUP
 
 app.post('/admin/signup', (req, res) => {
@@ -84,7 +100,7 @@ app.get('/admin/courses', adminAuthentication, (req, res) => {
 // USER SIGNUP
 
 app.post('/user/signup', (req, res) => {
-    const user = {...req.body, purchasedCourses: []}  // this is the spread syntax -> it essentially takes all properties of the req.body and adds them to new object
+    const user = {...req.body, purchasedCourses: []}  // this is the spread syntax -> it essentially takes all properties of the req.body and adds them to new object, here purchasedCourse array is to store the courses that the user has purchased.
 
     /* 
         the above one line is equal to the below four lines of code. Google it.
@@ -95,7 +111,61 @@ app.post('/user/signup', (req, res) => {
             purchasedCourses: []
         }
     */
+
+        USERS.push(user);
+        res.json({message:'signed up successfully'})
+});
+
+
+// USER LOGIN
+
+app.post('/user/login', userAuthentication, (req, res) => {
+    res.json({message:'logged in successfully'});
+}); 
+
+// GET COURSES OF THE USER
+
+app.get('/user/courses', userAuthentication, (req, res) => {
+
+    // we go into the COURSES array and check for the courses with property published in it.
+    res.json({courses: COURSES.filter(c => c.published)});
 })
+
+
+// USER COURSE PURCHASED
+
+app.post('/user/courses/:courseId', userAuthentication, (req, res) => {
+    const courseId = parseInt(req.params.courseId);
+    const course = COURSES.find(c => c.id === courseId && c.published);
+    if(course){
+
+        // we used the req.user in userAuthenticatio, now we are using that info here to change the user infornmation in the global USERS array.
+        req.user.purchasedCourses.push(courseId);
+        res.json({message:'Course purchased successfully'});
+    }else{
+        res.status(403).json({message:'Course not found or available'})
+    }
+})
+
+// GET USERS PURCHASED COURSES
+
+app.get('/user/purchasedCourses', userAuthentication, (req, res) => {
+    // we need to extract the complete course object from the COURSES which have ids which are present in eq.user.purchasedCourses
+    const purchasedCourses = COURSES.filter(c => req.user.purchasedCourses.includes(c.id));
+
+    // an alternate way of doing it
+
+  /*   var purchasedCoursesId = [];
+    for(let i = 0; i < COURSES.length; i++){
+        if(purchasedCoursesId.indexOf(COURSES[i].id) != -1){
+            purchasedCourses.push(COURSES[i]);
+        }
+    } */
+    res.json({purchasedCourses});
+});
+
+
+
 app.listen(3000, () => {
     console.log('successfully started at 3000');
 })
