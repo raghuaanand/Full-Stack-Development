50f87d8296d77fdf26e5436167670c1ca3fe95a4
Raghu Anand <raghuaanand@gmail.com>
2024-01-25 10:57:26 +0530

    adding frontend

---
+++ Week 5/NEW-YEAR-APP/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,8 @@
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="title"></input>
+        <input type="text" placeholder="placeholder"></input>
+
+        <button>Add a todo</button>
+    </div>
+}
