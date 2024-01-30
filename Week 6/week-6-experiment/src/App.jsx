06c3f4cc36764fc2209c5fe7e011b247d5592dc4
Raghu Anand <raghuaanand@gmail.com>
2024-01-30 06:51:24 +0530

    pushing state down in react

--- Week 6/week-6-experiment/src/App.jsx
+++ Week 6/week-6-experiment/src/App.jsx
@@ -1,34 +1,31 @@
 import React, {Fragment, useState} from "react"
 
 function App() {
-  const [name, setName] = useState('Raghu');
-
-  function handleNameChange(){
-    setName (Math.random());
-  }
   return (
-    // we can do <React.Fragment> </React.Fragment> instead of just <></> both does the same thing it does not insert an extra div 
     <Fragment>
-      <button onClick={handleNameChange}>Click me to change the name</button>  
-      <Header title = {name}/>
+    <HeaderWithButton />
       <Header  title="Anand"/>
     </Fragment>
   )
 }
 
-//  the fucntion below can also destructure the object this way
+// now when we code like this whole components does not render, only the component with the useState hook renders.
+//  this makes the application slightly more optimal.
 
-/* 
+function HeaderWithButton(){
+  const [title, setTitle] = useState("Raghu");
 
-function Header(props){
-  return (
+  return(
     <div>
-      {props.title}
+      <button onClick={() => {
+        setTitle(Math.random());
+      }}>Click to change the title </button>
+      <Header title={title}/>
     </div>
-  )
+    )
 }
 
- */
+
 
 function Header({title}){
   return (
