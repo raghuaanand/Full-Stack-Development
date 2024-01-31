ab8f38e0d17a8d332277ec23db1d0dda9e22ced7
Raghu Anand <raghuaanand@gmail.com>
2024-01-31 23:29:10 +0530

    memo working

--- Week 6/week-6-experiment/src/App.jsx
+++ Week 6/week-6-experiment/src/App.jsx
@@ -1,32 +1,30 @@
-import React, { memo, useState} from "react"
+import { useState } from "react"
+import { memo } from 'react';
 
 function App() {
+  const [firstTitle, setFirstTitle] = useState("my name is harkirat");
 
-  const [title, setTitle] = useState("Raghu");
-
-  function handleTitle(){
-    setTitle(Math.random());
+  function changeTitle() {
+    setFirstTitle("My name is " + Math.random())
   }
+
   return (
-    <>
-      <button onClick={handleTitle}>Click to change the title </button>
-      <Header title={title}/>
-      <Header  title="Anand"/>
-      <Header  title="Anand"/>
-      <Header  title="Anand"/>
-    </>
+    <div>
+      <button onClick={changeTitle}>Click me to change the title</button>
+      <Header title={firstTitle} />
+      <br />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+    </div>
   )
 }
 
-//  memo lets you skip re-rendering the components when its props are unchanged, that is what we were doing by pushing the state down in previous commit.
-
-
-const Header = React.memo(function Header({title}){
-  return (
-    <div>
-      {title}
-    </div>
-  )
-}) 
+const Header = memo(function ({title}) {
+  return <div>
+    {title}
+  </div>
+})
 
 export default App
