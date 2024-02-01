0766fb0ae59606831afe42bbdb829f934df2fa98
Raghu Anand <raghuaanand@gmail.com>
2024-02-01 17:44:49 +0530

    keys added

---
+++ Week 6/TODO/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+      <input type='text' placeholder='Title'/>
+      <input type='text' placeholder='Descritption'/>
+    </div>
+  )
+}
+
+export default Todo
