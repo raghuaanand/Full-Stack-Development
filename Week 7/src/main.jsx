0f2f7ab2e434319690019d18b9ad48e1c4440f12
Raghu Anand <raghuaanand@gmail.com>
2024-02-29 23:42:33 +0530

    prop drilling added

--- Week 7/src/main.jsx
+++ Week 7/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import PropDrilling from './PropDrilling'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PropDrilling />
   </React.StrictMode>,
   document.getElementById('root')
 )
