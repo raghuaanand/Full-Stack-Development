9a41506d0fce73997b924dccbc30db3b3074d742
Raghu Anand <raghuaanand@gmail.com>
2024-03-11 13:48:34 +0530

    types in ts

--- Week 9/typescript/src/a.ts
+++ Week 9/typescript/src/a.ts
@@ -1,4 +1,4 @@
-interface User {
+/* interface User {
 	firstName: string;
 	lastName: string;
 	email?: string;
@@ -18,4 +18,40 @@ isLegal({
     lastName: 'Anand',
     email: 'raghuaanand@gmail.com',
     age: 21
-})
+}) */
+
+
+//  implementing interfaces
+
+/* interface Person{
+    name: String,
+    age: number,
+    greet(phrase: string): void // a funciton named greet that takes string as input and returns void
+}
+
+// this above interface can be used to implement classes
+
+class Manager implements Person{
+    name: String;
+    age: number;
+
+    constructor(n: string, a: number){
+        this.name = n;
+        this.age = a;
+    }
+
+    greet(phrase: string): void {
+        console.log(`${phrase} ${this.name}`);
+    }
+
+}
+
+const e = new Manager('raghu', 21); */
+
+//  types in typescript
+
+type User = {  // = is not put in interfaces  // check the slides at https://daily-code-web.vercel.app/tracks/6SbPPXGkG8QKFOTW9BmL/ts-7
+    firstname: String,
+    email: String,
+    age: number
+}
