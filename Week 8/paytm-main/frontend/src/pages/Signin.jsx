c1edf69b1fd1e808a0d580653821d84ee00ae888
Raghu Anand <raghuaanand@gmail.com>
2024-03-15 08:22:50 +0530

    sign up page added

---
+++ Week 8/paytm-main/frontend/src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../components/Heading'
+
+const Signin = () => {
+  return (
+    <div>
+      <Heading label={"Sign in"}/>
+    </div>
+  )
+}
+
+export default Signin
